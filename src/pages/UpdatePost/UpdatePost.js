e0522f25ce025fdb18144fe7d3f0074c60811064
Nikhil Sharma <nikhilsharmamusic2000@gmail.com>
2021-09-26 17:13:36 +0530

    [FrontEnd] Added Update Post Page

---
+++ src/pages/UpdatePost/UpdatePost.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./UpdatePost.css";
+import Form from "../../components/Form/Form";
+import ImageSource from "./../../shared/ImageSource";
+
+const UpdatePost = () => {
+  return (
+    <div className="updatepost__container">
+      <Form style={{ width: "45rem" }}>
+        <div className="form__banner">
+          <h1>Update Post</h1>
+        </div>
+        <div className="form__body">
+          <div className="updatepost__textarea">
+            <textarea placeholder="Type your post"></textarea>
+          </div>
+          <div className="updatepost--image__picker">
+            <img src={ImageSource.homeBanner} alt="upload image" />
+            <label htmlFor="updatepost__image--picker">Update Image</label>
+            <input
+              type="file"
+              id="updatepost__image--picker"
+              style={{ display: "none" }}
+              accept=".jpg,.png,.jpeg"
+            />
+          </div>
+          <button type="submit" className="form__button">
+            Update
+          </button>
+        </div>
+      </Form>
+    </div>
+  );
+};
+
+export default UpdatePost;
