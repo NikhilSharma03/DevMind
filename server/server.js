8757614e49b96429a74cb50db1ffe82ee8d72fad
Nikhil Sharma <nikhilsharmamusic2000@gmail.com>
2021-11-11 14:01:48 +0530

    connected to mongodb

--- server/server.js
+++ server/server.js
@@ -1,17 +1,26 @@
 const express = require("express")
 const app = express()
 const bodyParser = require("body-parser")
+const mongoose = require("mongoose")
+
+// Middlewares
+require('dotenv').config()
+app.use(bodyParser.json())
 
 // Routers
 const postRoutes = require("./routes/posts")
 
-// Middlewares
-app.use(bodyParser.json())
-
 // Routes
 app.use("/api/posts", postRoutes)
 
-// Listen on port 5000
-app.listen(process.env.PORT || 5000, () => {
-    console.log("Server is running...")
+
+// Db connection
+mongoose.connect(process.env.MONGO_DB_URL).then(() => {
+    console.log("Connected to Database...")
+    // Listen on port 5000
+    app.listen(process.env.PORT || 5000, () => {
+        console.log("Server is running...")
+    })
+}).catch(err => {
+    console.log(err)
 })
