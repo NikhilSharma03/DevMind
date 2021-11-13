95615e34c3f4132b3b47a4a93839d6e9b675180e
Nikhil Sharma <nikhilsharmamusic2000@gmail.com>
2021-11-13 18:01:46 +0530

    added /api/comments routes

---
+++ server/controllers/comments.js
@@ -0,0 +1,3 @@
+const Comment = require("./../models/comments")
+const Post = require("./../models/posts")
+const User = require("./../models/users")
---
+++ server/routes/comments.js
@@ -0,0 +1,9 @@
+const express = require("express")
+const router = express.Router()
+// Post controllers
+const commentsController = require("./../controllers/comments")
+
+// Method handlers
+
+
+module.exports = router
