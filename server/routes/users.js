dc7c86c33eb55f218d53aa553affab894a49c34e
Nikhil Sharma <nikhilsharmamusic2000@gmail.com>
2021-11-13 17:11:00 +0530

    added user router

---
+++ server/routes/users.js
@@ -0,0 +1,9 @@
+const express = require("express")
+const router = express.Router()
+// Post controllers
+const usersController = require("./../controllers/users")
+
+// Method handlers
+
+
+module.exports = router
