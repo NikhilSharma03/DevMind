c77e8f54369127b9e519876f4fc423ed28f0c87e
Nikhil Sharma <nikhilsharmamusic2000@gmail.com>
2021-11-25 23:44:18 +0530

    added object ID verification

--- server/controllers/likes.js
+++ server/controllers/likes.js
@@ -1,3 +1,7 @@
 exports.postLikeHandler = async (req, res) => {
-
+    const {userID} = req.body
+    let isObjectID = isValidObjectId(userID)
+    if (!isObjectID) {
+        return res.status(404).json({message:"the provided id is invalid"})
+    }
 }
