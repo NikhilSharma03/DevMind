a71aebbf42f0e87f1067524fce45f4388855c4ab
Nikhil Sharma <nikhilsharmamusic2000@gmail.com>
2021-12-07 17:26:22 +0530

    fix drawer

--- client/src/components/SideDrawer/SideDrawer.js
+++ client/src/components/SideDrawer/SideDrawer.js
@@ -1,9 +1,14 @@
 import React from "react";
 import "./SideDrawer.css";
 import NavItem from "./../NavItem/NavItem";
-import ImageSource from "./../../shared/ImageSource";
+import { useSelector } from "react-redux"
+import ImageSource from "./../../shared/ImageSource"
 
 function SideDrawer(props) {
+  const username = useSelector(state => state.user.username)
+  const email = useSelector(state => state.user.email)
+  const profileImage = useSelector(state => state.user.profileImage)
+
   return (
     <div
       className="sidedrawer__container"
@@ -13,10 +18,10 @@ function SideDrawer(props) {
     >
       <div className="sidedrawer__profile">
         <figcaption className="sd__profile--imgcont">
-          <img alt="profile" />
+          <img src={process.env.REACT_APP_IMAGE_PATH+profileImage} alt="profile" />
         </figcaption>
-        <h1>Name</h1>
-        <p>Posts</p>
+        <h1>{username}</h1>
+        <p>{email}</p>
       </div>
       <nav className="sidedrawer__nav">
         <ul onClick={props.toggleSD} className="sd__nav--ul">
