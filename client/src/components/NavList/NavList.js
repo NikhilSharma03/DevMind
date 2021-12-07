db1648a204318dfe8019bc41eb7e8ac33383adbd
Nikhil Sharma <nikhilsharmamusic2000@gmail.com>
2021-12-07 17:31:58 +0530

    protected routes

--- client/src/components/NavList/NavList.js
+++ client/src/components/NavList/NavList.js
@@ -1,16 +1,16 @@
 import React from "react";
 import "./NavList.css";
 import NavItem from "./../NavItem/NavItem";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 
-const NavList = () => {
+const NavList = (props) => {
   const token = useSelector(state => state.user.token)
 
   return (
     <ul className="nav__ul">
-      <NavItem href="/feed">Feed</NavItem>
-      <NavItem href="/create_post">Create</NavItem>
-      <NavItem href="/my_profile">Profile</NavItem>
+      {token && <NavItem href="/feed">Feed</NavItem>}
+      {token && <NavItem href="/create_post">Create</NavItem>}
+      {token && <NavItem href="/my_profile">Profile</NavItem>}
       <NavItem href={token ? "/logout" : "/login"} login={true}>
         {token ? "LOGOUT" : "LOGIN"}
       </NavItem>
